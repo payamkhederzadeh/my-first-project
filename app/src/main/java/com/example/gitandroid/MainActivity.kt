d9bbec79ff739e425d3a627a16da3f9c48f64889
payamkhederzadeh <payamkhedezadeg@gmail.com>
2022-02-06 23:54:55 +0330

    some toast add2

--- app/src/main/java/com/example/gitandroid/MainActivity.kt
+++ app/src/main/java/com/example/gitandroid/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         android.widget.Toast.makeText(this, "salam", Toast.LENGTH_SHORT).show()
+        android.widget.Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show()
     }
 }
